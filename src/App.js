792b426fb31c08d767a5913db995e7f4b1ec43f1
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2022-09-07 12:46:34 +0600

    Homepage text-zone done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.scss';
 import Layout from './components/Layout'
 import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
    
       <Route path="/" element={ <Layout/> } />
+      <Route index element={<Home/>} />
     </Routes>
     
     
---
+++ src/components/Home/index.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react"
+import LogoTitle from "../../assets/images/logo-s.png"
+import AnimatedLetters from "../AnimatedLetters"
+import "./index.scss"
+import {Link} from 'react-router-dom'
+import Sidebar from "../Sidebar"
+
+const Home=()=>{
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray=['m','a','e','e','s','h','a']
+    const jobArray= [ 'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',]
+
+useEffect(()=>{
+    setTimeout(()=>{
+        setLetterClass('text-animate-hover')
+    }, 4000)
+
+}, [])
+
+
+
+    return( 
+        <> 
+         <Sidebar/>
+
+    <div className="container home-page">
+
+        <div className="text-zone">
+            <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>                                      
+                 <br/> 
+                 <span className={`${letterClass} _13`}>I</span>    
+                 <span className={`${letterClass} _14`}>'m</span> 
+            <img src={LogoTitle} alt="developer" />
+           <AnimatedLetters letterClass={letterClass} strArray={nameArray}  idx={15}/>
+
+            <br/>
+            <AnimatedLetters letterClass={letterClass} strArray={jobArray}  idx={22}/>
+             </h1>
+             <h2> Frontend Devloper / Javascript Expert/ Lecturer </h2>
+             <Link to='/contact' className="flat-button"> Contact ME</Link>
+        </div>
+
+    </div>
+    </>
+    );
+}
+
+export default Home
